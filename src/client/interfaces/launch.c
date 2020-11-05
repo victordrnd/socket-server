6fa16543167a523ab85afe30ec4dff846551c535
Compte Etudiant <student@localhost.localdomain>
2020-11-05 15:52:03 +0100

    handler v1

--- src/client/interfaces/launch.c
+++ src/client/interfaces/launch.c
@@ -1,16 +1,47 @@
 #include "launch.h"
+
+#include <stdbool.h>
+#include <gtk/gtk.h>
+
 GtkBuilder *builder = NULL;
+
+
+void on_mise_10_selected(GtkWidget *button)
+{
+    GtkWidget  *rbtn_label;
+
+    rbtn_label = gtk_radio_button_new_with_label (builder, "mise de 10");
+
+    printf("%s\n", gtk_button_get_label(&rbtn_label));
+}
+
+void on_mise_25_selected()
+{
+}
+
+void on_mise_50_selected()
+{
+}
+
+void on_mise_100_selected()
+{
+}
+
+void btn_collab_toggled_cb()
+{
+    GtkButton *btn = GTK_BUTTON(gtk_builder_get_object(builder, "btn_collab"));
+    printf("%s", gtk_button_get_label(&btn));
+}
 
 void btn_trahir_toggled_cb()
 {
     GtkButton *btn = GTK_BUTTON(gtk_builder_get_object(builder, "btn_trahir"));
     printf("%s", gtk_button_get_label(&btn));
-
 }
 
 void init_main_window(int argc, char **argv){
     GtkWidget *win;
-
+    
     gtk_init(&argc, &argv);
     builder = gtk_builder_new_from_file("src/client/ressources/glade/interface_prisonnier.glade");
     win = GTK_WIDGET(gtk_builder_get_object(builder, "app_win"));
--- src/client/interfaces/launch.h
+++ src/client/interfaces/launch.h
@@ -1,8 +1,35 @@
 #include <gtk/gtk.h>
+
 #ifndef LAUNCH_HEADER_H
 #define LAUNCH_HEADER_H
 
+typedef struct 
+{
+    // bool rb_state;
+    char rb_label;
 
+} GtkRadioButton;
+
+typedef struct
+{
+    
+
+} toggle_button;
+
+//Boutons radios
+void on_mise_10_selected(GtkWidget *button);
+void on_mise_25_selected();
+void on_mise_50_selected();
+void on_mise_100_selected();
+
+//Boutons toggleds
+void btn_collab_toggled_cb();
+void btn_trahir_toggled_cb();
+
+//Plateau du jeu
 void init_main_window(int argc, char **argv);
+
+//Glade builder
 GtkBuilder *get_gtk_builder();
+
 #endif
