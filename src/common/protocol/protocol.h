91b390ec61bfdb5efe748fb5c82233804962e04a
Compte Etudiant <student@localhost.localdomain>
2020-11-12 09:15:22 +0100

    structure protocol fait

--- src/common/protocol/protocol.h
+++ src/common/protocol/protocol.h
@@ -2,6 +2,7 @@
 #define PROTOCOL_H
 
 enum verbs{
+
     CONNECT = 0x10,
     CONNECTED = 0x11,
     GAME_START = 0x12,
@@ -9,7 +10,16 @@ enum verbs{
     ROUND_END = 0x14,
     GAME_END = 0x15,
     DISCONNECT = 0x16
-
 };
+
+typedef struct {
+
+    int sender_id;
+    int destination_id;
+    int timestamp;
+    enum verbs action;
+    char binary[2048];
+    
+} Encapsulation;
 
 #endif
